3a217d8a5a000cc737180026427d75dce5c1ead3
Tamme Schichler <tamme@schichler.dev>
2021-07-11 20:19:44 +0200

    Cleaned up and fully implemented `key_deserializer`

--- src/de/key_deserializer.rs
+++ src/de/key_deserializer.rs
@@ -1,5 +1,5 @@
 use super::{Error, ReportAt, Result};
-use serde::de;
+use serde::{de, forward_to_deserialize_any};
 use taml::{diagnostics::Reporter as diagReporter, parsing::Key};
 
 pub struct KeyDeserializer<'a, 'de, Position: Clone, Reporter: diagReporter<Position>> {
@@ -10,214 +10,6 @@ impl<'a, 'de, Position: Clone, Reporter: diagReporter<Position>> de::Deserialize
 	for KeyDeserializer<'a, 'de, Position, Reporter>
 {
 	type Error = Error;
-
-	fn deserialize_any<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		self.deserialize_identifier(visitor)
-	}
-
-	fn deserialize_bool<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_i8<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_i16<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_i32<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_i64<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_u8<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_u16<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_u32<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_u64<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_f32<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_f64<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_char<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_str<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		visitor
-			.visit_str(self.key.name.as_ref())
-			.report_at(self.reporter, self.key.span)
-	}
-
-	fn deserialize_string<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		visitor
-			.visit_str(self.key.name.as_ref())
-			.report_at(self.reporter, self.key.span)
-	}
-
-	fn deserialize_bytes<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_byte_buf<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_option<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_unit<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_unit_struct<V>(self, name: &'static str, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_newtype_struct<V>(self, name: &'static str, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_seq<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_tuple<V>(self, len: usize, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_tuple_struct<V>(
-		self,
-		name: &'static str,
-		len: usize,
-		visitor: V,
-	) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_map<V>(self, visitor: V) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_struct<V>(
-		self,
-		name: &'static str,
-		fields: &'static [&'static str],
-		visitor: V,
-	) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
-
-	fn deserialize_enum<V>(
-		self,
-		name: &'static str,
-		variants: &'static [&'static str],
-		visitor: V,
-	) -> Result<V::Value>
-	where
-		V: de::Visitor<'de>,
-	{
-		todo!()
-	}
 
 	fn deserialize_identifier<V>(self, visitor: V) -> Result<V::Value>
 	where
@@ -232,6 +24,19 @@ impl<'a, 'de, Position: Clone, Reporter: diagReporter<Position>> de::Deserialize
 	where
 		V: de::Visitor<'de>,
 	{
-		todo!()
+		visitor.visit_unit().report_at(self.reporter, self.key.span)
+	}
+
+	fn deserialize_any<V>(self, visitor: V) -> Result<V::Value>
+	where
+		V: de::Visitor<'de>,
+	{
+		self.deserialize_identifier(visitor)
+	}
+
+	forward_to_deserialize_any! {
+		bool i8 i16 i32 i64 i128 u8 u16 u32 u64 u128 f32 f64 char str string
+		bytes byte_buf option unit unit_struct newtype_struct seq tuple
+		tuple_struct map struct enum
 	}
 }
