7cbbb9cd541db8729c4561f0ed735e25ddbf6616
Tamme Schichler <tamme@schichler.dev>
2021-07-12 13:29:26 +0200

    A comma

--- tests/deserializer.rs
+++ tests/deserializer.rs
@@ -117,7 +117,7 @@ fn deserializer() {
 		dbg!(from_taml_str::<Deserializable, _>(
 			input,
 			&mut reporter,
-			&[("UTF-8", &|str| Ok(Cow::Borrowed(str)))]
+			&[("UTF-8", &|str| Ok(Cow::Borrowed(str)))],
 		))
 		.tap_err(|_| {
 			let mut codemap = CodeMap::new();
